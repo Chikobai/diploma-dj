431c69d985ef111dc04198344e583996d7b698b9
mukhitabdinazarov <mukhitabdinazarov@gmail.com>
2020-04-09 15:59:30 +0600

    changed default server email

--- project/email_info.py
+++ project/email_info.py
@@ -1,5 +1,5 @@
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mukhitabdinazarov@gmail.com'
-EMAIL_HOST_PASSWORD = 'mukhitsdu123'
+EMAIL_HOST_USER = 'jibergroupdev@gmail.com'
+EMAIL_HOST_PASSWORD = '4jibergroup'
 EMAIL_USE_TLS = True
