6e7e07370c71e1edcd64dd70c267be9999359677
mukhitabdinazarov <mukhitabdinazarov@gmail.com>
2020-04-10 19:04:23 +0600

    added reply api

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LessonViewSet
+from .views import LessonViewSet, QuestionAnswersViewSet
 
 router = DefaultRouter()
 router.register(r'course/modules/(?P<module_id>.+)/lessons', LessonViewSet, "lessons")
+router.register(r'question', QuestionAnswersViewSet, "questions_")
 
 urlpatterns = [
     path('', include(router.urls)),
--- lessons/views.py
+++ lessons/views.py
@@ -1,5 +1,9 @@
-from rest_framework import viewsets, permissions
-from .models import Lesson
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework import permissions
+from rest_framework.response import Response
+
+from .models import Lesson, Question, Answer, LessonTaker
 from .serializers import LessonSerializer
 
 
@@ -12,5 +16,71 @@ class LessonViewSet(viewsets.ModelViewSet):
         id = self.kwargs['module_id']
         querysets = Lesson.objects.filter(module=id)
         return querysets
+
+
+class QuestionAnswersViewSet(viewsets.ViewSet):
+
+    @action(detail=False, methods=['post'], permission_classes=[permissions.AllowAny])
+    def reply(self, request):
+        user = request.user
+        data = request.data
+        lesson_id = data.get('lesson', None)
+        question_id = data.get('question', None)
+        answer_id = data.get('answer', None)
+
+        if lesson_id is None:
+            return self.get_response_data(False, 'lesson объязательное поле', status.HTTP_400_BAD_REQUEST)
+
+        if question_id is None:
+            return self.get_response_data(False, 'question объязательное поле', status.HTTP_400_BAD_REQUEST)
+
+        if answer_id is None:
+            return self.get_response_data(False, 'answer объязательное поле', status.HTTP_400_BAD_REQUEST)
+
+        try:
+            lesson = Lesson.objects.get(pk=lesson_id)
+        except Lesson.DoesNotExist:
+            lesson = None
+
+        try:
+            question = Question.objects.get(pk=question_id)
+        except Question.DoesNotExist:
+            question = None
+
+        try:
+            answer = Answer.objects.get(pk=answer_id)
+        except Answer.DoesNotExist:
+            answer = None
+
+        if lesson is None:
+            return self.get_response_data(False, 'Не найден такой урок', status.HTTP_400_BAD_REQUEST)
+
+        if question is None:
+            return self.get_response_data(False, 'Не найден такой вопрос', status.HTTP_400_BAD_REQUEST)
+
+        if answer is None:
+            return self.get_response_data(False, 'Не найден такой ответ', status.HTTP_400_BAD_REQUEST)
+
+        if question.lesson != lesson:
+            return self.get_response_data(False, 'Вопрос не соответсвует к уроку', status.HTTP_400_BAD_REQUEST)
+
+        if answer.question != question:
+            return self.get_response_data(False, 'Ответ не соответсвует к вопросу', status.HTTP_400_BAD_REQUEST)
+
+        taker, created = LessonTaker.objects.get_or_create(user=user, lesson=lesson)
+
+        if answer.is_true:
+            asd = taker.correct_answers+1
+            taker.correct_answers = asd
+            taker.save()
+            return self.get_response_data(True, 'Првильный ответ', status.HTTP_200_OK)
+        else:
+            return self.get_response_data(False, 'Неверный ответ', status.HTTP_200_OK)
+
+    def get_response_data(self, _success, _message, _status):
+        response_data = dict()
+        response_data['success'] = _success
+        response_data['message'] = _message
+        return Response(response_data, status=_status)
 
 
